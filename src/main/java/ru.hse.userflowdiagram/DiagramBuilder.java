8b3efda8661628526bd7cdf7eadd72e3971a6bfb
Nikita Usoltsev <57728940+usoltsev37@users.noreply.github.com>
2021-11-05 15:47:39 +0300

    MVP: simple graph without visualization, parse only compulsory tags, list actions is not complete

---
+++ src/main/java/ru.hse.userflowdiagram/DiagramBuilder.java
@@ -0,0 +1,241 @@
+package ru.hse.userflowdiagram;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DiagramBuilder {
+
+    private final String URL_STR;
+    private final String fileName;
+    private final String tab;
+    private final Document doc;
+
+    public DiagramBuilder(String URL_STR,  String fileName, String tab) {
+        Document doc;
+        this.URL_STR = URL_STR;
+        this.fileName = fileName;
+        this.tab = tab;
+        try {
+            doc = Jsoup.connect(URL_STR).get();
+        } catch (IOException e) {
+            doc = null;
+            e.printStackTrace(System.err);
+        }
+        this.doc = doc;
+    }
+
+    public void build() {
+        StringBuilder strBuildResult = new StringBuilder();
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+
+            strBuildResult.append(getTitle());
+
+            strBuildResult.append(getAdress());
+            strBuildResult.append(getAudios());
+            strBuildResult.append(getVideos());
+            strBuildResult.append(getButtons());
+            strBuildResult.append(getDetails());
+            strBuildResult.append(getDialogs());
+            strBuildResult.append(getFieldsets());
+            strBuildResult.append(getInputs());
+            strBuildResult.append(getMenuitems());
+            strBuildResult.append(getProgress());
+            strBuildResult.append(getSelect());
+            strBuildResult.append(getTables());
+            strBuildResult.append(getTextarea());
+
+
+
+            writer.write(strBuildResult.toString());
+        } catch (IOException e) {
+            e.printStackTrace(System.err);
+        }
+    }
+
+    private String getAdress() {
+        return getElements("adress", "Adress");
+    }
+
+    private String getAudios() {
+        return getElements("audio", "Audios");
+    }
+
+    private String getVideos() {
+        return getElements("video", "Videos");
+    }
+
+    private String getButtons() {
+        String tag = "button";
+        String title = "Buttons";
+        Elements elements = doc.select(tag);
+        StringBuilder strBuilder = new StringBuilder();
+        strBuilder.append(String.format("%s %s (%d):\n", tab, title, elements.size()));
+        for (Element element : elements) {
+            String name = "EMPTY_NAME";
+            if (!element.attr("aria-label").isEmpty()) {
+                name = element.attr("aria-label");
+            } else if (!element.attr("label").isEmpty()) {
+                name = element.attr("label");
+            }
+
+            String id = "EMPTY_ID";
+            if (!element.attr("id").isEmpty()) {
+                id = element.attr("id");
+            } else if (!element.attr("class").isEmpty()) {
+                id = element.attr("class");
+            }
+
+            String type = "EMPTY_TYPE";
+            if (!element.attr("type").isEmpty()) {
+                type = element.attr("type");
+            }
+
+            strBuilder.append(String.format("%s%s * %s [id: %s, type %s]\n", tab, "\t", name, id, type));
+        }
+        if (elements.isEmpty()) {
+            strBuilder.append(String.format("%s%s No %s on the page\n", tab, "\t", title));
+        }
+        return strBuilder.toString();
+    }
+
+    private String getDetails() {
+        return getElements("details", "Details");
+    }
+
+    private String getDialogs() {
+        return getElements("dialog", "Dialogs");
+    }
+
+    private String getFieldsets() {
+        return getElements("fieldset", "Fieldsets");
+    }
+
+    private String getInputs() {
+        String tag = "input";
+        String title = "Inputs";
+        Elements elements = doc.select(tag);
+        StringBuilder strBuilder = new StringBuilder();
+        strBuilder.append(String.format("%s %s (%d):\n", tab, title, elements.size()));
+        for (Element element : elements) {
+            String type = element.attr("type");
+            List<String> actions = new ArrayList<>();
+            switch (type) {
+                case ("text") :
+                    break;
+                case ("password") :
+                    break;
+                case ("radio") :
+                    String nameRadio = element.attr("name");
+                    String valueRadio = element.attr("value");
+                    if (nameRadio.isEmpty()) {
+                        nameRadio = "EMPTY_NAME";
+                    }
+                    if (valueRadio.isEmpty()) {
+                        valueRadio = "EMPTY_VALUE";
+                    }
+                    actions.add(String.format("Turn %s on value %s", nameRadio, valueRadio));
+                    actions.add(String.format("Turn %s off value %s", nameRadio, valueRadio));
+                    break;
+                case ("checkbox") :
+                    String nameCheckbox = element.attr("name");
+                    if (nameCheckbox.isEmpty()) {
+                        nameCheckbox = element.attr("id");
+                    }
+                    if (nameCheckbox.isEmpty()) {
+                        nameCheckbox = "EMPTY_NAME";
+                    }
+                    actions.add(String.format("Turn on %s", nameCheckbox));
+                    actions.add(String.format("Turn off %s", nameCheckbox));
+                    break;
+                case ("hidden") :
+                    String nameHidden = element.attr("name");
+                    if (nameHidden.isEmpty()) {
+                        nameHidden = element.attr("id");
+                    }
+                    if (nameHidden.isEmpty()) {
+                        nameHidden = "EMPTY_NAME";
+                    }
+                    actions.add(String.format("Click on %s", nameHidden));
+                    break;
+                case ("button") :
+                    break;
+                case ("submit") :
+                    break;
+                case ("reset") :
+                    break;
+                case ("image") :
+                    break;
+                case ("file") :
+                    break;
+                case ("email") :
+                    actions.add("Enter email address");
+                    break;
+                case ("search") :
+                    actions.add("Enter text to search");
+                    break;
+                default:
+                    return getElements(tag, title);
+            }
+
+
+            strBuilder.append(String.format("%s%s * %s -- Acrions: ", tab, "\t", type)).append(actions).append("\n");
+        }
+        if (elements.isEmpty()) {
+            strBuilder.append(String.format("%s%s No %s on the page\n", tab, "\t", title));
+        }
+        return strBuilder.toString();
+    }
+
+    private String getMenuitems() {
+        return getElements("menuitem", "Menuitems");
+    }
+
+    private String getProgress() {
+        return getElements("progress", "Progress");
+    }
+
+    private String getSelect() {
+        return getElements("select", "Select");
+    }
+
+    private String getTables() {
+        return getElements("table", "Tables");
+    }
+
+    private String getTextarea() {
+        return getElements("textarea", "Textarea");
+    }
+
+    private String getTitle() {
+        Elements titles = doc.select("title");
+        if (titles.isEmpty()) {
+            return String.format("Site: %s\n", URL_STR);
+        } else {
+            return String.format("Site: %s\n", titles.get(0).text());
+        }
+    }
+
+    private String getElements(String tag, String title) {
+        Elements elements = doc.select(tag);
+        StringBuilder strBuilder = new StringBuilder();
+        strBuilder.append(String.format("%s %s (%d):\n", tab, title, elements.size()));
+        for (Element element : elements) {
+            strBuilder.append(String.format("%s%s * ", tab, "\t")).append(element).append("\n");
+        }
+        if (elements.isEmpty()) {
+            strBuilder.append(String.format("%s%s No %s on the page\n", tab, "\t", title));
+        }
+        return strBuilder.toString();
+    }
+
+
+
+}
---
+++ src/test/java/ru/hse/userflowdiagram/TestMain.java
@@ -0,0 +1,41 @@
+package ru.hse.userflowdiagram;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TestMain {
+    static String OZON_URL = "https://www.ozon.ru";
+    static String OZON_FILE = "/Users/thebest/Documents/HSE/semester_5/JB-UserFlowDiagram/src/test/java/ru/hse/userflowdiagram/output/ozon";
+
+    static String FARFETCH_URL = "https://www.farfetch.com/ru/";
+    static String FARFETCH_FILE = "/Users/thebest/Documents/HSE/semester_5/JB-UserFlowDiagram/src/test/java/ru/hse/userflowdiagram/output/farfetch";
+
+    static String LAMODA_URL = "https://www.lamoda.ru";
+    static String LAMODA_FILE = "/Users/thebest/Documents/HSE/semester_5/JB-UserFlowDiagram/src/test/java/ru/hse/userflowdiagram/output/lamoda";
+
+    static String YUOTUBE_URL = "https://www.youtube.com";
+    static String YUOTUBE_FILE = "/Users/thebest/Documents/HSE/semester_5/JB-UserFlowDiagram/src/test/java/ru/hse/userflowdiagram/output/youtube";
+
+    static String VARLAMOV_URL = "https://varlamov.ru";
+    static String VARLAMOV_FILE = "/Users/thebest/Documents/HSE/semester_5/JB-UserFlowDiagram/src/test/java/ru/hse/userflowdiagram/output/varlamov";
+
+    static String WYLSA_URL = "https://wylsa.com";
+    static String WYLSA_FILE = "/Users/thebest/Documents/HSE/semester_5/JB-UserFlowDiagram/src/test/java/ru/hse/userflowdiagram/output/wylsa";
+
+    static List<SiteData> URLS_FILES = new ArrayList<>(Arrays.asList(
+            new SiteData(OZON_URL, OZON_FILE),
+            new SiteData(FARFETCH_URL, FARFETCH_FILE),
+            new SiteData(LAMODA_URL, LAMODA_FILE),
+            new SiteData(YUOTUBE_URL, YUOTUBE_FILE),
+            new SiteData(VARLAMOV_URL, VARLAMOV_FILE),
+            new SiteData(WYLSA_URL, WYLSA_FILE)
+            ));
+
+    public static void main(String[] args) {
+        for (var site : URLS_FILES) {
+            DiagramBuilder builder = new DiagramBuilder(site.url, site.file, "\t");
+            builder.build();
+        }
+    }
+}
